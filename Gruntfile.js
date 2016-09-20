727b4a892922773811d5f388dfdbcce23b453619
feelman <feelman@list.ru>
2016-09-20 15:00:23 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,176 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html",
+            "*.php"
+          ],
+          dest: "build"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+      php: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: ["*.php"],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    },
+
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "src/less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 1 version",
+              "last 2 Chrome versions",
+              "last 2 Firefox versions",
+              "last 2 Opera versions",
+              "last 2 Edge versions"
+            ]})
+          ]
+        },
+        src: "build/css/*.css"
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    svgstore: {
+      options: {
+        svg: {
+          style: "display: none"
+        }
+      },
+      symbols: {
+        files: {
+          "build/img/symbols.svg": ["src/img/*.svg"]
+        }
+      }
+    },
+
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: ["build/img/*.svg"]
+        }]
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif}"]
+        }]
+      }
+    },
+
+    php: {
+        dist: {
+            options: {
+                hostname: 'localhost',
+                port: 3000,
+                base: 'build', // Project root
+                keepalive: false,
+                open: false
+            }
+        }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "*.php",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: "build",
+          watchTask: true,
+          notify: false,
+          open: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["src/*.html"],
+        tasks: ["copy:html"]
+      },
+      php: {
+        files: ["src/*.php"],
+        tasks: ["copy:php"]
+      },
+      style: {
+        files: ["src/less/**/*.less"],
+        tasks: ["less", "postcss", "csso"],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["php:dist", "browserSync", "watch"]);
+  grunt.registerTask("symbols", ["svgmin", "svgstore"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "csso",
+    "symbols",
+    "imagemin"
+  ]);
+};
---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+  </head>
+  <body>
+    <header class="page-header">
+
+    </header>
+    <main>
+      <div class="page-main">
+        
+      </div>
+    </main>
+    <footer class="page-footer">
+
+    </footer>
+    <script src="js/script.js"></script>
+  </body>
+</html>
